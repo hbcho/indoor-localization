09c1d83fe0a323b3d77a5eb4b81138f0f4c806ee
Jack Weinkselbaum <Jack@lawn-128-61-71-15.lawn.gatech.edu>
2019-03-07 10:15:13 -0500

    Updated rssi_to_dist

--- location_calculation.py
+++ location_calculation.py
@@ -205,10 +205,20 @@ def rssi_to_dist(proccessed_live_rssi_data):
     '''
     # TODO: implement the algorithm from the link above
     txPower = -54 # According to the spec of the beacon
+    returnList = []
+    for i in range(len(proccessed_live_rssi_data)):
+        curr = proccessed_live_rssi_data[i]
+        if curr == 0:
+            returnList.append(-1.0)
+        else:
+            ratio = curr * 1.0 / txPower
+            if ratio < 1.0:
+                returnList.append(math.pow(ratio,10))
+            else:
+                returnList.append((0.89976) * math.pow(ratio,7.7095) + 0.111)
 
     # return dist_to_beacons
-    raise NotImplementedError
-
+    return returnList
 
 def perform_trilateration_with_live_data(distances):
     '''Perform trilateration calculation
